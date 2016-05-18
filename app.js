406d7613c3b50d8ab2050ee83497bfef36cbeb9b
Tobias Gillström <tejb54@gmail.com>
2016-05-18 16:52:23 +0200

    Working on multilayer for multiplayer

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ var _ = require('lodash');
 
 var players = {};
 
+//dictonary containing battle objects index with battleRoomId
+var battles = {};
+
+var monsterObj = function (name, hp, maxHp, level) {
+    this.name = name;
+    this.hp = hp;
+    this.maxhp = maxHp;
+    this.level = level;
+};
+
+var playerObj = function (id,monster,alive,dead) {
+  this.id = id; //id for this player
+  this.monster = monster; //holds the current selected monster for this player
+  this.alive = alive;
+  this.dead = dead;
+};
+
+var battle = function () {
+    this.players = []; //contains player objects
+    this.currentPlayerTurn = ''; //this will hold the id for current player
+};
+
+
 io.on('connection',function (socket) {
     console.log('connection');
 
@@ -23,7 +46,7 @@ io.on('connection',function (socket) {
 
         //send already connected players back to myself
         _.forEach(players,function (item) {
-            if(item.room == player.room)
+            if(item.room == player.room && item.id != player.id)
             {
                 socket.emit('spawn',item);
             }
@@ -44,6 +67,8 @@ io.on('connection',function (socket) {
 
     socket.on('moving',function (data) {
         data.id = socket.id;
+        players[socket.id].x = data.x;
+        players[socket.id].y = data.y;
         socket.broadcast.to(players[socket.id].room).emit('moving',data);
     });
 
@@ -56,7 +81,63 @@ io.on('connection',function (socket) {
         }
     });
 
-    socket.on('interact', function (otherPlayer) {
+    socket.on('interact', function (id) {
+        console.log("interaction started by " + socket.id +" to " + id);
+        socket.broadcast.to(id).emit('interact_request',socket.id);
+    });
+
+    socket.on('interact_response',function (data) {
+        console.log("player " + socket.id + " has responded with " + data.res );
+
+        if(data.res)
+        {
+            var room = "battle_id";
+            socket.broadcast.to(data.id).emit('interact_response',room);
+            socket.emit('interact_response',room);
+        }
+
+    });
+
+
+    socket.on('start_battle',function (obj) {
+        var battleRoomId = obj.battleId;
+
+        socket.join(battleRoomId);
+
+        //if the room has not been created
+        if(!battles[battleRoomId])
+        {
+            battles[battleRoomId] = new battle();
+        }
+        else
+        {
+            //get already connected player info
+            socket.emit('enter_battle', battles[battleRoomId].players[0]);
+
+            //send your info to the other player
+            socket.broadcast.to(battleRoomId).emit('enter_battle',new playerObj(socket.id,obj.monster,obj.alive,obj.dead));
+        }
+        battles[battleRoomId].players.push(new playerObj(socket.id,obj.monster,obj.alive,obj.dead));
+
+        //should preform a calculation to set the current player depending on speed
+        //now the player that joined last is the current player.
+        battles[battleRoomId].currentPlayerTurn = socket.id;
+    });
+
+    socket.on('attack_battle',function (obj) {
         
+    });
+
+    socket.on('change_monster', function (obj) {
+
+    });
+
+    socket.on('leave_battle',function (battleRoomId) {
+
+        //this needs to be fixed so that you only set room to null if all players have left
+        battles[battleRoomId] = null;
+
+        //tell socket.io to leave the room
+        socket.leave(battleRoomId);
     });
 });
